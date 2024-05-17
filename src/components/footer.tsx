abe168da84f7fc22db74268432c6cc13315e68d9
Jeongmin Lee <clcl6084@gmail.com>
2024-05-18 00:19:07 +0900

    feat: implement routing with footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,70 @@
 import { IconHome } from "@/icons/Icon_home";
-import { IconMyPage } from "@/icons/Icon_mypage";
+import { IconMyPage } from "@/icons/Icon_myPage";
+
 import { IconSettings } from "@/icons/Icon_settings";
+import useMenuStore, { Menu } from "@/store/menuStore";
+import { useRouter } from "next/router";
 
 // 선택되면 text-primary: #2DD197
 // 선택안되면 text-darkGray: #848A8A
 export default function Footer() {
+  const { currentMenu, setCurrentMenu } = useMenuStore();
+  const route = useRouter();
+
+  const isCurrentMenu = (menu: string) => {
+    return currentMenu === menu;
+  };
+
+  const handleRoute = (menu: Menu) => {
+    setCurrentMenu(menu);
+    route.push(`/${menu === "home" ? "" : menu}`);
+  };
+
   return (
     <div className="mx-[30px] h-[60px] flex relative">
-      <div className="bg-red-200 rounded-full me-[80px] w-full items-center justify-around flex">
-        <div className="flex items-center justify-center flex-col gap-[4px]">
-          <IconHome />
-          <p className="text-darkGray text-[10px]">홈</p>
-        </div>
-        <div className="flex items-center justify-center flex-col gap-[4px]">
-          <IconMyPage />
-          <p className="text-darkGray text-[10px]">마이</p>
-        </div>
-        <div className="flex items-center justify-center flex-col gap-[4px]">
-          <IconSettings />
-          <p className="text-darkGray text-[10px]">설정</p>
-        </div>
+      <div className="bg-white rounded-full me-[80px] w-full items-center justify-around flex">
+        <button
+          className="flex items-center justify-center flex-col gap-[4px]"
+          type="button"
+          onClick={() => handleRoute("home")}
+        >
+          <IconHome isActive={isCurrentMenu("home")} />
+          <p
+            className={`${
+              isCurrentMenu("home") ? "text-primary" : "text-darkGray"
+            } text-[10px]`}
+          >
+            홈
+          </p>
+        </button>
+        <button
+          className="flex items-center justify-center flex-col gap-[4px]"
+          type="button"
+          onClick={() => handleRoute("mypage")}
+        >
+          <IconMyPage isActive={isCurrentMenu("mypage")} />
+          <p
+            className={`${
+              isCurrentMenu("mypage") ? "text-primary" : "text-darkGray"
+            } text-[10px]`}
+          >
+            마이
+          </p>
+        </button>
+        <button
+          className="flex items-center justify-center flex-col gap-[4px]"
+          type="button"
+          onClick={() => handleRoute("setting")}
+        >
+          <IconSettings isActive={isCurrentMenu("setting")} />
+          <p
+            className={`${
+              isCurrentMenu("setting") ? "text-primary" : "text-darkGray"
+            } text-[10px]`}
+          >
+            설정
+          </p>
+        </button>
       </div>
       <div className="absolute end-0 bg-primary w-[60px] h-[60px] flex flex-col gap-[4px] items-center justify-center rounded-full text-white">
         <p className="text-[16px]">+</p>
