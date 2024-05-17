944a133074e8c8fd1787a4de50359e2e38953a63
Jeongmin Lee <clcl6084@gmail.com>
2024-05-18 02:50:24 +0900

    feat: implement list page wit dummy

---
+++ src/components/mypage/List_myEvent.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Map_detailOverlay from "../Map/Map_detailOverlay";
+
+const events = [
+  {
+    id: 1,
+    imageURL: "/images/잠수교.png",
+    title: "차 없는 잠수교 뚜벅뚜벅 축제",
+    description:
+      "2024 차 없는 잠수교 뚜벅뚜벅 축제의 오감으로 만나는 힐링 놀이터로 놀러와 힐링을 동시에 즐겨보세요.",
+    startAt: new Date("2022-01-01T00:00:00"),
+    finishAt: new Date("2022-01-01T01:00:00"),
+  },
+  {
+    id: 2,
+    imageURL: "/images/잠수교.png",
+    title: "차 없는 잠수교 뚜벅뚜벅 축제",
+    description:
+      "2024 차 없는 잠수교 뚜벅뚜벅 축제의 오감으로 만나는 힐링 놀이터로 놀러와 힐링을 동시에 즐겨보세요.",
+    startAt: new Date("2022-01-02T00:00:00"),
+    finishAt: new Date("2022-01-02T01:00:00"),
+  },
+  // Add more events as needed
+];
+
+const List_myEvent = () => {
+  return (
+    <div className="flex flex-col gap-[16px]">
+      {events.map((event) => (
+        <Map_detailOverlay
+          key={event.id}
+          title={event.title}
+          description={event.description}
+          startAt={event.startAt}
+          finishAt={event.finishAt}
+          imageURL={event.imageURL}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default List_myEvent;
