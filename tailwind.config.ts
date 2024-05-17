45075487f538527a5e4e95c1ce0e292f7a6c3c97
Jeongmin Lee <clcl6084@gmail.com>
2024-05-18 00:15:36 +0900

    chore: add primary-10

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,7 @@ const config: Config = {
       ...colors,
       primary: {
         DEFAULT: "#2DD197",
-        10: "#FFF0D9",
+        10: "#D5F9EC",
         20: "#FFE7C4",
         30: "#FFE0B5",
         40: "#FED9A6",
